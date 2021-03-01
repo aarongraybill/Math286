f4ff1678f60e08e1b9dd92fea495f58a155df828
aarongraybill <agraybill@haverford.edu>
2021-03-01 11:04:20 -0500

    finishing 3/1

--- R-scripts/SimultaneousExample.R
+++ R-scripts/SimultaneousExample.R
@@ -16,3 +16,17 @@ confint(out,"confidence",level=.95)
 confint(out,"confidence",level=.9)
 
 
+#i.
+confint(out,level = 1-.1/2)
+
+#j.
+data=data.frame(Size=c(30,65,100))
+prediction <- 
+  predict(out,data,interval="confidence",se.fit=T,level=.9)
+
+ses <- 
+  sqrt(12.4^2+prediction$se.fit^2)
+
+W <- sqrt(2*qf(1-.1,2,23))
+
+
